68f1de768dd0419288439bbc67de554cbbae81b0
RyanHarang <ryh110.rh@gmail.com>
2025-02-16 12:57:14 -0800

    moving to inline svg

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,27 @@
-import filter from "../../assets/svg/profile/list.svg";
-
 function Filter({ open, toggle }) {
   return (
     <div
       onClick={toggle}
       className="border-light-fg dark:border-dark-fg hover:bg-light-bg2 dark:hover:bg-dark-bg2 flex h-[3rem] w-[4rem] min-w-[4rem] cursor-pointer items-center justify-center rounded-r-lg border-l-2"
     >
-      <img src={filter} alt="Filter options" className="w-[2.25rem]" />
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        role="img"
+        aria-labelledby="filterDesc"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="stroke-light-fg dark:stroke-dark-fg w-[2.25rem]"
+      >
+        <desc id="filterDesc">A list icon for showing filter options</desc>
+        <path
+          d="M16 12H42M16 24H42M16 36H42M6 12H6.02M6 24H6.02M6 36H6.02"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </div>
   );
 }
