8d37bf250997fbab39bbe44f6983e2d66e09c257
RyanHarang <ryh110.rh@gmail.com>
2025-02-16 12:56:45 -0800

    removing excess styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
 import AppRoutes from "./AppRoutes.jsx";
 
 function App() {
   return (
     <>
-      <AppRoutes className="h-screen" />
+      <AppRoutes />
     </>
   );
 }
