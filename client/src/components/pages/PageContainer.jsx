8b37f61e323bac6706bee98353cb6ceb271d5d75
RyanHarang <ryh110.rh@gmail.com>
2025-03-04 12:24:23 -0800

    Added Footer to page container

--- client/src/components/pages/PageContainer.jsx
+++ client/src/components/pages/PageContainer.jsx
@@ -1,10 +1,12 @@
 import Navigation from "../Navigation/Navigation.jsx";
+import Footer from "../Footer/Footer.jsx";
 
 function PageContainer({ children }) {
   return (
     <>
       <Navigation />
       <div className="min-h-screen min-w-screen pt-20">{children}</div>
+      <Footer />
     </>
   );
 }
