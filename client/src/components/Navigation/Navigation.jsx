46005eea9b387d833657ac6e36800e92a57980a4
RyanHarang <ryh110.rh@gmail.com>
2025-03-04 18:41:23 -0800

    Styling navigation links

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -6,22 +6,31 @@ import LoginIcon from "../../assets/svg/profile/LoginIcon.jsx";
 
 function Navigation() {
   return (
-    <header className="bg-light-bg/90 dark:bg-dark-bg/90 fixed z-50 inline-flex h-[10vh] min-h-[50px] w-full items-center justify-between p-4 text-base leading-normal font-semibold">
+    <header className="bg-light-bg/90 dark:bg-dark-bg/90 fixed z-50 grid w-full grid-cols-3 p-4 text-base leading-normal font-semibold">
       <div className="flex cursor-pointer items-center justify-start gap-2">
         <LogoIcon />
       </div>
-      <div className="flex items-center justify-start gap-8">
-        <Link to="/" className="cursor-pointer">
+      <div className="flex items-center justify-center gap-8">
+        <Link
+          to="/"
+          className="after:bg-accent relative cursor-pointer after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:transition-all after:duration-400 hover:after:w-full"
+        >
           Drivers
         </Link>
-        <Link to="/Teams" className="cursor-pointer">
+        <Link
+          to="/Teams"
+          className="after:bg-accent relative cursor-pointer after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:transition-all after:duration-400 hover:after:w-full"
+        >
           Teams
         </Link>
-        <Link to="/Race" className="cursor-pointer">
+        <Link
+          to="/Race"
+          className="after:bg-accent relative cursor-pointer after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:transition-all after:duration-400 hover:after:w-full"
+        >
           Race
         </Link>
       </div>
-      <div className="flex items-center justify-end gap-3">
+      <div className="flex items-center justify-end gap-5">
         <ModeToggle />
         <ThemeToggle />
         <LoginIcon />
