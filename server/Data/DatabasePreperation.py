283aa7f423a2f34b7e2e65244c34bf8bc0410d29
Ian-McCormick <mccormi@wwu.edu>
2025-03-10 19:51:11 -0700

    Added nationality to drivers/teams

---
+++ server/Data/DatabasePreperation.py
@@ -0,0 +1,373 @@
+import os
+import json
+
+import DetailedScraper as laps
+from DriverScraper import Driver
+from TeamScraper import Team
+from RaceScraper import Race
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class AugmentedDriver:
+    def __init__(self, id, name, picture, number, DOB, nationality, natCode, lastYear, team, totalRaces, wins):
+        self.id = id
+        self.name = name
+        self.picture = picture
+        self.number = number
+        self.dateOfBirth = DOB
+        self.nationality = nationality
+        self.natCode = natCode
+        try:
+            self.lastYear = int(lastYear)
+        except:
+            self.lastYear = None
+        self.lastTeam = team
+        self.totalRaces = totalRaces
+        self.wins = wins
+
+    def print(self):
+        print("ID: " + str(self.id))
+        print("Name: " + str(self.name))
+        print("Picture: " + str(self.picture))
+        print("Number: " + str(self.number))
+        print("DOB: " + str(self.dateOfBirth))
+        print("Nationality: " + str(self.nationality))
+        print("Nation Code: " + str(self.natCode))
+        print("Last Year: " + str(self.lastYear))  # Fixed capitalization
+        print("Last Team: " + str(self.lastTeam))
+        print("Total Races: " + str(self.totalRaces))
+        print("Wins: " + str(self.wins))
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "image": self.picture,
+            "number": self.number,
+            "DOB": self.dateOfBirth,
+            "nationality": self.nationality,
+            "natCode": self.natCode,
+            "lastYear": self.lastYear,
+            "team": self.lastTeam,
+            "totalRaces": self.totalRaces,
+            "wins": self.wins
+        }
+    
+    def from_json(data):
+        return AugmentedDriver(
+            id = data["id"],
+            name = data["name"],
+            picture = data["image"],
+            number = data["number"],
+            DOB= data["DOB"],
+            nationality= data["nationality"],
+            natCode = data["natCode"],
+            lastYear= data["lastYear"],
+            team= data["team"],
+            totalRaces = data["totalRaces"],
+            wins = data["wins"]
+        )
+
+class AugmentedTeam:
+    def __init__(self, id, name, nationality, natCode,  picture, wins, races, drivers):
+        self.id = id
+        self.name = name
+        self.picture = picture
+        self.nationality = nationality
+        self.natCode = natCode
+        self.wins = wins
+        self.races = races
+        self.drivers = drivers
+    
+    def print(self):
+        print(f"ID: " + str(self.id))
+        print("Name: " + str(self.name))
+        print("Picture: " + str(self.picture))
+        print("Nationality: " + str(self.nationality))
+        print("Nation Code: " + str(self.natCode))
+        print("Wins: " + str(self.wins))
+        print("Races: " + str(self.races))
+        print("Drivers: " + str(self.drivers))
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "nationality": self.nationality,
+            "natCode": self.natCode,
+            "image": self.picture,
+            "wins": self.wins,
+            "races": self.races,
+            "drivers": self.drivers
+        }
+    
+
+    def from_json(data):
+        return AugmentedTeam(
+            id = data["id"],
+            name = data["name"],
+            picture = data["picture"],
+            nationality = data["nationality"],
+            natCode = data["natCode"],
+            wins = data["wins"],
+            races = data["races"],
+            drivers = data["drivers"]
+        )
+
+class AugmentedRace:
+    def __init__(self, id, title, date, track, winner, fastestLap, polePosition):
+        self.id = id
+        self.title = title
+        self.date = date
+        self.track = track
+        self.winner = winner
+        self.fastestLap = fastestLap
+        self.polePosition = polePosition
+    
+    def print(self):
+        print("ID: " + str(self.id))
+        print("Title: " + str(self.title))
+        print("Date: " + str(self.date))
+        print("Track: " + str(self.track))
+        print("Winner: " + str(self.winner))
+        print("Fastest Lap: " + str(self.fastestLap))
+        print("Pole Position: " + str(self.polePosition))
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "date": self.date,
+            "track": self.track,
+            "winner": self.winner,
+            "fastestLap": self.fastestLap,
+            "polePosition": self.polePosition
+        }
+    
+    def from_json(data):
+        return AugmentedRace(
+            id = data["id"],
+            title = data["title"],
+            date = data["date"],
+            track = data["track"],
+            winner = data["winner"],
+            fastestLap = data["fastestLap"],
+            polePosition = data["polePosition"]
+        )
+
+class AugmentedLap:
+    def __init__(self, race_id, driver_id, number, time, position):
+        self.raceId = race_id
+        self.driverId = driver_id
+        self.lapNumber = number
+        self.time = time
+        self.position = position
+    
+    def print(self):
+        print("Race ID: " + str(self.raceId))
+        print("Driver ID: " + str(self.driverId))
+        print("Lap Number: " + str(self.lapNumber))
+        print("Time: " + str(self.time))
+        print("Position: " + str(self.position))
+    
+    def to_dict(self):
+        return {
+            "raceId": self.raceId,
+            "driverId": self.driverId,
+            "lapNumber": self.lapNumber,
+            "time": self.time,
+            "position": self.position
+        }
+    
+    def from_json(data):
+        return AugmentedLap(
+            race_id = data["raceId"],
+            driver_id = data["driverId"],
+            number = data["lapNumber"],
+            time = data["time"],
+            position = data["position"]
+        )
+
+DRIVER_IDS = {}
+DRIVER_LAST_TEAM = {}
+TEAM_IDS = {}
+RACE_IDS = {}
+
+def augmentedDriverSetup():
+    filePath = CURRENT_DIRECTORY + "/JSON/driverData.json"
+    f_in = open(filePath, "r")
+    rawData = json.load(f_in)
+    f_in.close()
+    baseDrivers = [Driver.from_json(data) for data in rawData]
+    newDrivers = []
+    nextId = 1
+
+    for driver in baseDrivers:
+        print(driver.name)
+        new = AugmentedDriver(
+            id=nextId,
+            name=driver.name,
+            picture=driver.picture,
+            number=driver.number,
+            DOB=driver.dateOfBirth,
+            nationality = driver.nationality,
+            natCode = driver.natCode,
+            lastYear=driver.lastYear,
+            team = None,
+            totalRaces=driver.totalRaces,
+            wins=driver.wins
+        )
+        DRIVER_IDS[driver.name] = nextId
+        DRIVER_LAST_TEAM[driver.name] = driver.lastTeam
+        newDrivers.append(new)
+        nextId += 1
+
+    return newDrivers
+
+def augmentedTeamSetup():
+    filePath = CURRENT_DIRECTORY + "/JSON/TeamData.json"
+    f_in = open(filePath, "r")
+    rawData = json.load(f_in)
+    f_in.close()
+    baseTeams = [Team.from_json(data) for data in rawData]
+    newTeams = []
+    nextId = 1
+
+    for team in baseTeams:
+        print(team.name)
+        teamDrivers = team.drivers
+        teamDriverIds = []
+        for d in teamDrivers:
+            id = DRIVER_IDS[d]
+            if id is None:
+                print(f"ERROR {d} NOT FOUND")
+            else:
+                teamDriverIds.append(id)
+        
+        new = AugmentedTeam(
+            id = nextId,
+            name = team.name,
+            picture = team.picture,
+            nationality = team.nationality,
+            natCode = team.natCode,
+            wins = team.wins,
+            races = team.races,
+            drivers = teamDriverIds
+        )
+        TEAM_IDS[team.name] = nextId
+        newTeams.append(new)
+        nextId += 1
+
+    return newTeams
+
+def augmentedRaceSetup():
+    filePath = CURRENT_DIRECTORY + "/JSON/RaceData.json"
+    f_in = open(filePath, "r")
+    rawData = json.load(f_in)
+    f_in.close()
+    baseRaces = [Race.from_json(data) for data in rawData]
+    newRaces = []
+    nextId = 1
+
+    for race in baseRaces:
+        #print(race.title)
+        poleID = None
+        winnerID = None
+        try:
+            poleID = DRIVER_IDS[race.polePosition]
+        except:
+            print(f"{race.title} Has no pole driver")
+        try:
+            winnerID = DRIVER_IDS[race.winner]
+        except:
+            print(f"{race.title} Has no winner")
+
+        new = AugmentedRace(
+            id = nextId,
+            title = race.title,
+            date = race.date,
+            track = race.track,
+            winner = winnerID,
+            fastestLap = race.fastestLap,
+            polePosition = poleID
+        )
+        RACE_IDS[race.title] = nextId
+        nextId += 1
+        newRaces.append(new)
+
+    return newRaces
+
+def augmentedLapSetup():
+    path = CURRENT_DIRECTORY + "/JSON/"
+    files = [file for file in os.listdir(path) if "detailedRaces" in file]
+    newLaps = []
+    for file in files:
+        f_in = open(path + file, "r")
+        rawData = json.load(f_in)
+        f_in.close()
+        baseSessions = [laps.detailedSession.from_json(data) for data in rawData]
+        for session in baseSessions:
+            raceID = None
+            driverID = None
+            try:
+                raceID = RACE_IDS[session.title]
+            except:
+                print(f"race {session.title} has no attached ID")
+            try:
+                driverID = DRIVER_IDS[session.driver]
+            except:
+                print(f"driver {session.driver} has no attached ID")
+
+            for lap in session.laps:
+                newLaps.append(AugmentedLap(
+                    race_id=raceID,
+                    driver_id=driverID,
+                    number=lap.number,
+                    position=lap.position,
+                    time=lap.time
+                ))
+    
+    return newLaps       
+
+def main():
+    drivers = augmentedDriverSetup()
+    print("Drivers Finished\n--------------------------")
+    races = augmentedRaceSetup()
+    print("Races Finished\n--------------------------")
+    teams = augmentedTeamSetup()
+    print("Teams Finished\n--------------------------")
+    teamKeys = TEAM_IDS.keys()
+
+    #attach teams to drivers now that teams have IDs
+    for driver in drivers:
+        teamName = DRIVER_LAST_TEAM[driver.name]
+        if teamName is None:
+            print(f"Error {driver.name} has no last team")
+            return
+        
+        teamID = None
+        for key in teamKeys:
+            if teamName in key:
+                teamID = TEAM_IDS[key]
+                break
+        
+        if teamID is None:
+            print(f"Error {teamName} has no attached ID")
+
+        driver.lastTeam = teamID
+
+    laps = augmentedLapSetup()
+
+    driverDicts = [driver.to_dict() for driver in drivers]
+    raceDicts = [race.to_dict() for race in races]
+    teamDicts = [team.to_dict() for team in teams]
+    lapDicts = [lap.to_dict() for lap in laps]
+    all = {"drivers":driverDicts, "races":raceDicts, "teams":teamDicts, "laps":lapDicts}
+    keys = all.keys()
+    for key in keys:
+        f_out = open(CURRENT_DIRECTORY + f"/{key}FINAL.json", "w+")
+        f_out.write(json.dumps(all[key], indent=4))
+        f_out.close()
+
+main()
+print("done")
---
+++ server/Data/DetailedScraper.py
@@ -0,0 +1,212 @@
+import time
+import os
+import json
+import requests
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from typing import List
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class Driver:
+    def __init__(self, driver_id, name):
+        self.driver_id = driver_id
+        self.name = name
+
+    def to_json(self):
+        return {"driver_id": self.driver_id, "name": self.name}
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(driver_id=data["driver_id"], name=data["name"])
+
+class Race:
+    def __init__(self, race_id, title):
+        self.race_id = race_id
+        self.title = title
+        self.drivers:List[Driver] = []
+
+    def to_json(self):
+        return {
+            "race_id": self.race_id,
+            "title": self.title,
+            "drivers": [driver.to_json() for driver in self.drivers]
+        }
+
+    @classmethod
+    def from_json(cls, data):
+        race = cls(race_id=data["race_id"], title=data["title"])
+        race.drivers = [Driver.from_json(driver_data) for driver_data in data["drivers"]]  # Correct usage
+        return race
+
+class Season:
+    def __init__(self, ID, year):
+        self.ID = ID
+        self.year = year
+        self.races: List[Race]
+    
+    def toJson(self):
+        return{
+            "season_id":self.ID,
+            "year":self.year,
+            "races":[race.toJson() for race in self.races]
+        }
+    
+    @classmethod
+    def from_json(cls, data):
+        season = cls(ID = data["season_id"], year = data["year"])
+        season.races = [Race.from_json(race) for race in data.get("races", [])]
+        return season
+
+class Lap:
+    def __init__(self, no, pos, time):
+        self.number = no
+        self.position = pos
+        self.time = time
+
+    def toJson(self):
+        return{
+            "number":self.number,
+            "position":self.position,
+            "time":self.time
+        }
+    
+    def from_json(data):
+        return (Lap(
+            no = data["number"],
+            pos=data["position"],
+            time = data["time"]
+        ))
+
+class detailedSession:
+    def __init__(self, year, title, driver, laps):
+        self.year = year
+        self.title = title
+        self.driver = driver
+        self.laps:List[Lap] = laps
+
+    def toJson(self):
+        return{
+            "year":self.year,
+            "title":self.title,
+            "driver":self.driver,
+            "laps":[lap.toJson() for lap in self.laps]
+        }
+    
+    def from_json(data):
+        laps = [Lap.from_json(lap) for lap in data["laps"]]
+        return detailedSession(
+            year = data["year"],
+            title = data["title"],
+            driver = data["driver"],
+            laps=laps
+        )
+
+
+# options = webdriver.ChromeOptions()
+# allData = []
+# args = ["--headless", "--disable-gpu", "--window-size=1920x1080"]
+# for arg in args:
+#     options.add_argument(arg)
+
+# # Setup WebDriver (make sure you have chromedriver installed)
+# driver = webdriver.Chrome(options=options)
+# driver.get("https://pitwall.app/analysis/race-report")  # Replace with actual URL
+
+# wait = WebDriverWait(driver, 10)
+
+# def select_dropdown_option(dropdown_id, option_data_id):
+#     """Select an option from a custom dropdown menu by data-id."""
+#     dropdown = wait.until(EC.element_to_be_clickable((By.ID, dropdown_id)))
+#     dropdown.click()  # Open the dropdown
+#     option = wait.until(EC.element_to_be_clickable((By.XPATH, f"//span[@input-id='{dropdown_id.split('-')[-1]}' and @data-id='{option_data_id}']")))
+#     option.click()  # Select the option
+#     time.sleep(2)  # Allow time for next dropdown to update
+
+# def getIdentificationData():
+
+#     # Get all seasons
+#     season_options = driver.find_elements(By.XPATH, "//span[@input-id='season']")
+#     seasons:List[Season] = []
+#     for s in season_options:
+#         seasonID = s.get_attribute("data-id")
+#         year = s.get_attribute("textContent")
+#         seasons.append(Season(seasonID, year))
+
+#     for season in seasons:
+#         print(season.year)
+#         select_dropdown_option("dropdown-select-season", season.ID)
+
+#         # Get all races for the selected season
+#         race_options = driver.find_elements(By.XPATH, "//span[@input-id='race']")
+#         races: List[Race] = []
+
+#         for o in race_options:
+#             raceID = o.get_attribute("data-id")
+#             title = o.get_attribute("textContent")
+#             races.append(Race(raceID, title))
+
+#         for race in races:
+#             select_dropdown_option("dropdown-select-race", race.ID)
+
+#             # Get all drivers for the selected race
+#             driver_options = driver.find_elements(By.XPATH, "//span[@input-id='driver']")
+#             drivers = []
+#             for option in driver_options:
+#                 driverID = option.get_attribute("data-id")
+#                 name = option.get_attribute("textContent")
+#                 drivers.append(Driver(driverID, name))
+            
+#             race.drivers=drivers
+        
+#         season.races = races
+
+#     f_out = open(CURRENT_DIRECTORY + "/detailedRaces.json", "w+")
+#     json.dump([season.toJson() for season in seasons], f_out, indent=4)
+#     f_out.close()
+#     driver.quit()
+
+def getRaceDetails():
+    session = requests.session()
+    f_in = open(CURRENT_DIRECTORY + "/detailedRaces.json", "r")
+    rawData = json.load(f_in)
+    seasons = [Season.from_json(season) for season in rawData]
+    index = 1
+    for season in seasons:
+        allDetails = []
+        sID = season.ID
+        for race in season.races:
+            rID = race.race_id
+            for driver in race.drivers:
+                dID = driver.driver_id
+                print(f"{race.title} : {driver.name}")
+                try:
+                    result = session.get(f"https://pitwall.app/analysis/race-report?season={sID}&race={rID}&driver={dID}&button=")
+                    src = BeautifulSoup(result.content, "html.parser")
+                    table = src.find("table", {"class":"data-table"})
+                    rows = table.find("tbody").find_all("tr")
+                    laps = []
+                    for row in rows:
+                        dataPoints = row.find_all("td")
+                        no = dataPoints[0].text.strip()
+                        pos = dataPoints[1].text
+                        time = dataPoints[2].text
+                        laps.append(Lap(no, pos, time))
+                    allDetails.append(detailedSession(season.year, race.title, driver.name, laps))
+                except:
+                    continue
+
+        f_out = open(CURRENT_DIRECTORY + f"/JSON/detailedRaces{index}.json", "w+")
+        json.dump([s.toJson() for s in allDetails], f_out, indent=4)
+        f_out.close()
+        index += 1
+
+# start = time.time()
+# getRaceDetails()
+# end = time.time()
+# runtime = end-start
+# print(f"Completed after {runtime:.4f} seconds")
