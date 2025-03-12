8bc3931a8b3cea3b7f971508ac6ccc6a542755fa
RyanHarang <ryh110.rh@gmail.com>
2025-03-11 19:34:27 -0700

    updating routes to include home page

--- client/src/components/App/AppRoutes.jsx
+++ client/src/components/App/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "../pages/HomePage.jsx";
 import DriversPage from "../pages/DriversPage.jsx";
 import TeamsPage from "../pages/TeamsPage.jsx";
 import RacesPage from "../pages/RacesPage.jsx";
@@ -9,10 +10,11 @@ import SignupPage from "../pages/SignupPage.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <DriversPage />,
+    index: true,
+    element: <HomePage />,
   },
   {
-    path: "/DriversPage",
+    path: "/Drivers",
     element: <DriversPage />,
   },
   {
