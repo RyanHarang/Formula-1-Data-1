d8bb94d66db6b83471c957b2b6ab22113d14e9a6
RyanHarang <ryh110.rh@gmail.com>
2025-03-15 19:36:16 -0700

    styling

--- packages/client/src/components/Drivers/DriverModal.jsx
+++ packages/client/src/components/Drivers/DriverModal.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import "flag-icons/css/flag-icons.min.css";
 import noDriverIcon from "../../assets/svg/NoDriverImage.svg";
 
-const DriverModal = ({ handleCloseModal, natCode, name, teamName, DOB, lastYear, totalRaces, wins, image }) => {
+const DriverModal = ({
+  handleCloseModal,
+  natCode,
+  name,
+  teamName,
+  DOB,
+  lastYear,
+  totalRaces,
+  wins,
+  image,
+}) => {
   return (
     <div className="h-full w-full overflow-hidden">
       <div className="dark:bg-dark-bg2 bg-light-bg dark:border-accent relative h-full w-full overflow-hidden rounded-xl border-2 border-black">
         <div className="drag-handle dark:bg-dark-bg dark:border-accent relative flex cursor-grab items-center justify-start rounded-t-xl border-b-2 border-black bg-white text-black active:cursor-grabbing dark:text-white">
           <div className="border-accent rounded-tl-2xl py-0.75 pl-2.5">
             {natCode ? (
-              <span
-                className={`fi size-10 px-8 fi-${natCode}`}
-              ></span>
+              <span className={`fi size-10 px-8 fi-${natCode}`}></span>
             ) : (
               <span className="text-sm text-gray-400">N/A</span>
             )}
@@ -38,7 +46,7 @@ const DriverModal = ({ handleCloseModal, natCode, name, teamName, DOB, lastYear,
                 {teamName || "Unknown Team"}
               </p>
             </div>
-            <div className="dark:bg-dark-bg h-full rounded-lg bg-white p-3 shadow-md overflow-y-auto">
+            <div className="dark:bg-dark-bg h-full overflow-y-auto rounded-lg bg-white p-3 shadow-md">
               <h3 className="text-l text-dark-bg dark:border-accent mx-auto mb-4 w-[80%] border-b border-b-1 border-black text-left dark:text-white">
                 Driver Bio
               </h3>
@@ -48,9 +56,7 @@ const DriverModal = ({ handleCloseModal, natCode, name, teamName, DOB, lastYear,
                     DOB
                   </h2>
                   <p className="text-dark-bg dark:text-light-bg2 text-xl">
-                    {DOB
-                      ? DOB.split("(")[0]
-                      : "DOB not available"}
+                    {DOB ? DOB.split("(")[0] : "DOB not available"}
                   </p>
                 </div>
                 <div>
