7b80845360957bcf9aa9b0859cf1b887deb0b16b
RyanHarang <ryh110.rh@gmail.com>
2025-03-16 19:54:07 -0700

    Fixing height

--- packages/client/src/components/Teams/TeamCard.jsx
+++ packages/client/src/components/Teams/TeamCard.jsx
@@ -34,8 +34,7 @@ const TeamCard = ({
   return (
     <div
       onClick={() => onTeamClick(team.id)}
-      className="dark:border-accent hover:border-accent dark:bg-dark-bg2 flex cursor-pointer flex-col rounded-lg border-2 border-black shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] transition-transform duration-200 hover:scale-105"
-      style={{ height: "30vh" }}
+      className="dark:border-accent hover:border-accent dark:bg-dark-bg2 flex h-[250px] cursor-pointer flex-col rounded-lg border-2 border-black shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] transition-transform duration-200 hover:scale-105"
     >
       <div className="relative h-full w-full overflow-hidden rounded-t-md">
         {onAddFavorite && isAuthenticated && (
